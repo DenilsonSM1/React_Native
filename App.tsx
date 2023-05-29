0859020dac4a6be8ec4242af4793a1659f0c7504
DenilsonSM1 <luiz2017.lvmb@gmail.com>
2023-05-29 19:42:31 -0300

    Configuranco o meu babel

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste</Text>
+      <Text>Deus abençoe</Text>
       <StatusBar style="auto" />
     </View>
   );
