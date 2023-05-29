ad4e7226999fc70eebf44530d937fb507ac03360
DenilsonSM1 <luiz2017.lvmb@gmail.com>
2023-05-29 20:45:13 -0300

    Aqui importamos as fonts, se as fonts nao forem carregadas elas rodam o appLoading

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
 
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins";
 
+import {DMSans_400Regular} from "@expo-google-fonts/dm-sans";
+import {DMSerifDisplay_400Regular} from "@expo-google-fonts/dm-serif-display"
 
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular
+
+    })
+    if(!fontsLoaded){
+        return <AppLoading/>
+    }
   return (
     <View style={styles.container}>
-      <Text>Deus abençoe</Text>
-      <Text>ola</Text>
+      <Text>Carteira app</Text>
+
       <StatusBar style="auto" />
     </View>
   );
